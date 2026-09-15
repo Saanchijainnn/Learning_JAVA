d1f11831d36ca6b1ed11bd96c7c68b7367d5567f
Saanchi Jain <saanchi2315@gmail.com>
2026-09-15 23:53:14 +0530

    loops

--- src/com/ok/learningjava/datatypes/Test.java
+++ src/com/ok/learningjava/datatypes/Test.java
@@ -24,7 +24,7 @@ package com.ok.learningjava.datatypes;
     }
 }*/
 
-public class Test {
+/*public class Test {
     public static void main(String[] args) {
 
         int marks = 75;
@@ -37,4 +37,77 @@ public class Test {
             System.out.println("Grade C");
         }
     }
-}
+}*/
+
+/*public class Test {
+    public static void main(String[] args) {
+
+        int day = 2;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}*/
+
+/*public class Test {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        String result = (age >= 18) ? "Eligible to vote" : "Not eligible to vote";
+
+        System.out.println(result);
+    }
+}*/
+
+// LOOPS
+
+//while loop
+/*public class Test {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println("hello");
+            i++;
+        }
+    }
+}*/
+
+//For loop
+/*public class Test {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}*/
+
+//do while loop
+/*public class Test {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 5);
+    }
+} */
