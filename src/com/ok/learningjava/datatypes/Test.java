9fc7abb062634c40bc108ce827db35e47c77382e
Saanchi Jain <saanchi2315@gmail.com>
2026-09-15 21:24:01 +0530

    if, else if conditions

--- src/com/ok/learningjava/datatypes/Test.java
+++ src/com/ok/learningjava/datatypes/Test.java
@@ -1,1 +1,40 @@
+package com.ok.learningjava.datatypes;
 
+/*public class Test {
+     static void main(String[] args) {
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote");
+        }
+    }
+}*/
+
+/*public class Test {
+     static void main(String[] args) {
+
+        int age = 16;
+
+        if (age >= 18) {
+            System.out.println("You can vote");
+        } else {
+            System.out.println("You cannot vote");
+        }
+    }
+}*/
+
+public class Test {
+    public static void main(String[] args) {
+
+        int marks = 75;
+
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } else if (marks >= 60) {
+            System.out.println("Grade B");
+        } else {
+            System.out.println("Grade C");
+        }
+    }
+}
